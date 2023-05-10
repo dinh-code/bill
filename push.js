cc4d3e7b4976d9e69211dccf6c09fdac6e274d6e
dinh-code <63176375+dinh-code@users.noreply.github.com>
2023-05-11 01:12:51 +0700

    update

--- push.js
+++ push.js
@@ -8,9 +8,9 @@ const bill = {
     dataImport: () => _dataImport(),
 
     container: () => _container(),
-    info: () => _info(),
-    detail: (id, data) => _detail(id, data),
-    shop: () => _shop(),
+    info: (id, data) => _info(id, data),
+    detail: (data) => _detail(data),
+    shop: (id, data) => _shop(id, data),
 
     build: () => _build(),
 
@@ -37,11 +37,7 @@ function _container(id = 1){
     return container;
 }
 function _info(id = 0, data=[]){
-    //let soLg = data[1];
-    //let tienHang = bill.priceFormat(data[2]*1000);
     //let ghiChu = data[3];
-    //let ds = data[0];
-
     let info = document.createElement('div');
     info.classList.add("info");
     info.innerHTML =
@@ -49,19 +45,19 @@ function _info(id = 0, data=[]){
             <div class="info-top-left">
                 <div class="card h">
                     <div class="card-header"><i class="material-icons">person</i> Khách hàng (anh / chị):&nbsp;
-                        <div id="hoTen" class="card-body">---</div>
+                        <div class="card-body hoTen">${data[0]}</div>
                     </div>
                 </div>
                 <div class="card h">
                     <div class="card-header"><i class="material-icons">phone</i>Điện thoại:&nbsp;
-                        <div id="sdt" class="card-body">---</div>
+                        <div id="sdt" class="card-body">${data[1].length > 0 ? data[1] : '---'}</div>
                     </div>
                 </div>
                 <div class="card">
                     <div class="card-header">
                         <i class="material-icons">pin_drop</i>
                         <!--div class="card-body dc" style="width: 16mm;">Địa chỉ:&nbsp;</div-->
-                        <div id="diaChi" class="card-body dc">---</div>
+                        <div id="diaChi" class="card-body dc">Địa chỉ: ${data[2]}</div>
                     </div>
                 </div>
             </div>
@@ -69,31 +65,31 @@ function _info(id = 0, data=[]){
                 <div class="info-top-right-col1">
                     <div class="card">
                         <div class="card-header">Số hóa đơn</div>
-                        <div id="key" class="card-body-sm">---</div>
+                        <div id="key" class="card-body-sm">${data[3]}</div>
                     </div>
                     <div class="card">
                         <div class="card-header">Ngày đặt</div>
-                        <div id="ngayDat" class="card-body-sm">---</div>
+                        <div id="ngayDat" class="card-body-sm">${data[4]}</div>
                     </div>        
                 </div>
                 <div class="info-top-right-col2">
                     <div class="card text-right">
                         <div class="card-header text-j-end">Vận chuyển</div>
-                        <div id="tenDVVC" class="card-body-sm">---</div>
+                        <div id="tenDVVC" class="card-body-sm">${data[5]}</div>
                     </div>
                     <div class="card text-right">
                         <div class="card-header text-j-end">Mã vận đơn</div>
-                        <div id="maDon" class="card-body-sm">---</div>
+                        <div id="maDon" class="card-body-sm">${data[6]}</div>
                     </div>        
                 </div>
-                <div class="info-top-right-qrcode w17" id="keyQr">
-                </div>
+                <div class="info-top-right-qrcode w17" id="keyQr${id}"></div>
             </div>
         </div>
     `;
+    
     return info;
 }
-function _detail(id = 0, data=[]){
+function _detail(data=[]){
     let soLg = data[1];
     let tienHang = bill.priceFormat(data[2]*1000);
     let ghiChu = data[3];
@@ -148,13 +144,15 @@ function _detail(id = 0, data=[]){
 
     return detail;
 }
-function _shop(id = 1){
+function _shop(id = 0, data = []){
+    //let vcb = "https://img.vietqr.io/image/VCB-9968747831-qr_only.png?amount="+data[2]*1000+"&addInfo="+"23050500"+"%20"+removeVietnameseTones(data[3])+"%20"+bill.thongTin[1]+"&accountName=Dinh phuoc an";
+
     let shop = document.createElement('div');
     shop.classList.add("shop");
     shop.innerHTML =
         `<div class="col">
             <div class="info-top-right-qrcode">
-                <img id="vcb" src="">
+                <img src="${"https://img.vietqr.io/image/VCB-9968747831-qr_only.png?amount="+data[2]*1000+"&addInfo="+"23050500"+"%20"+removeVietnameseTones(data[3])+"%20"+bill.thongTin[1]+"&accountName=Dinh phuoc an"}">
             </div>
             <div class="payInfo">
                 <div class="headImg">
@@ -166,7 +164,7 @@ function _shop(id = 1){
             </div>
         </div>
         <div class="col">
-            <div class="info-top-right-qrcode" id="momo">
+            <div class="info-top-right-qrcode" id="momo${id}">
             </div>
             <div class="payInfo">
                 <div class="headImg">
@@ -186,21 +184,29 @@ function _shop(id = 1){
                     Mua hàng & tư vấn<BR>05696 08118 (Zalo)
                 </div>
             </div>
-            <div class="info-top-right-qrcode" id="gmap"></div>
+            <div class="info-top-right-qrcode" id="gmap${id}"></div>
         </div>`;
     return shop;
 }
 function _build(){
+    let id = 0;
     bill.donHang.forEach(d => {
         let container = bill.container();
-        let info = bill.info();
-        let detail = bill.detail(0, d);
-        let shop = bill.shop();
+        let info = bill.info(id, bill.thongTin);
+        let detail = bill.detail(d);
+        let shop = bill.shop(id, d);
         
         container.appendChild(info);
         container.appendChild(detail);
         container.appendChild(shop);
         document.body.appendChild(container);
+
+        new QRCode(document.getElementById("keyQr" + id), {text: bill.thongTin[3]+'', width: bill.qrSize, height: bill.qrSize});
+        new QRCode(document.getElementById("gmap" + id), {text: "https://goo.gl/maps/WNVStknA7ZxCAQG48", width: bill.qrSize, height: bill.qrSize});
+        //new QRCode(document.getElementById("momo" + id), {text: "2|99|0968747831|Dinh Phuoc An||0|0|"+d[2]*1000+"|"+bill.thongTin[3]+" "+bill.thongTin[0]+" "+bill.thongTin[1]+"|transfer_myqr", width: bill.qrSize, height: bill.qrSize});
+        //console.log("2|99|0968747831|Dinh Phuoc An||0|0|"+d[2]*1000+"|"+bill.thongTin[3]+" "+bill.thongTin[0]+" "+bill.thongTin[1]+"|transfer_myqr");
+
+        id++;
     });
 }
 function _dataImport(){
@@ -217,6 +223,7 @@ function _dataImport(){
     bill.matHang = mh;
     bill.donHang = j.dh;
     bill.thongTin = j.tt;
+    bill.thongTin[6] = bill.thongTin[6].length > 0 ? bill.thongTin[6] : '---';
 }
 function _priceFormat(n = 0){
     //return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(n);
