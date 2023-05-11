b640e4a74aca240f6c8ad2129c1c570a55a3a0a2
dinh-code <63176375+dinh-code@users.noreply.github.com>
2023-05-11 14:00:27 +0700

    Update push.js

--- push.js
+++ push.js
@@ -148,7 +148,7 @@ function _detail(data=[]){
             <td class="text-center">${item.ma}</td>
             <td>
                 <div class="card f-center flex h-fit">
-                    <div class="card-body">${item.ten}</div>
+                    <div class="card-body">${item.ten.length > 20 ? '*' + item.ten.substring(item.ten.length - 19) : item.ten.length}</div>
                     <div class="card-header">${item.moTa}</div>
                 </div>
             </td>
